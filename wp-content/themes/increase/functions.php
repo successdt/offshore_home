f0e0d801b3a78204fe551610c85c2eb3f2bf4e62
thanhdd <thanhdd@lifetimetech.vn>
2013-11-12 18:50:43 +0700

    debug

--- wp-content/themes/increase/functions.php
+++ wp-content/themes/increase/functions.php
@@ -167,9 +167,9 @@ function cmsms_font_style_list() {
 }
 
 remove_action('wp_head', 'wp_print_scripts');
-remove_action('wp_head', 'wp_print_head_scripts', 9);
+//remove_action('wp_head', 'wp_print_head_scripts', 9);
 remove_action('wp_head', 'wp_enqueue_scripts', 1);
 add_action('wp_footer', 'wp_print_scripts', 5);
 add_action('wp_footer', 'wp_enqueue_scripts', 5);
-add_action('wp_footer', 'wp_print_head_scripts', 5);
+//add_action('wp_footer', 'wp_print_head_scripts', 5);
 
