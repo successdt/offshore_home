0c600e85a10c5807c844e85e1f9c8f9c336456e8
thanhdd <thanhdd@lifetimetech.vn>
2013-11-12 18:28:18 +0700

    move js to footer

--- wp-content/themes/increase/functions.php
+++ wp-content/themes/increase/functions.php
@@ -166,3 +166,10 @@ function cmsms_font_style_list() {
 	return $list;
 }
 
+remove_action('wp_head', 'wp_print_scripts');
+remove_action('wp_head', 'wp_print_head_scripts', 9);
+remove_action('wp_head', 'wp_enqueue_scripts', 1);
+add_action('wp_footer', 'wp_print_scripts', 5);
+add_action('wp_footer', 'wp_enqueue_scripts', 5);
+add_action('wp_footer', 'wp_print_head_scripts', 5);
+
